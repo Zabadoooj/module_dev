65390fdde9a90c4f1d36acf5e9040991fc52e687
Zabadooj <51298182+Zabadoooj@users.noreply.github.com>
2025-10-01 17:18:20 +0300

    react-upd

--- module_dev-messenger/src/App.tsx
+++ module_dev-messenger/src/App.tsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
+type InputProps = {
+  alias: String;
+  textAlias: String;
+  defaultValue?: String;
+}
+
+type ButtonProps = {
+  title: String;
+  backroungColor?: String;
+}
+
+const Input = (props: InputProps) => {
+  return(
+    <div className="input">
+      <div className="input-name">
+        {props.alias}
+      </div>
+      <div className="inputbox" contentEditable>
+        {props.textAlias}
+      </div>
+    </div>
+  )
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <div className="button">
+      {props.title}
+      
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(32)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="film-creation-form">
+        <div className="text">Text</div>
+        <Input alias={"Логин"} textAlias={"Введите логин"}/>
+        <Input alias={"Почта"} textAlias={"Введите почту"}/>
+        <Input alias={"Пароль"} textAlias={"Введите пароль"}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count < 32 ? count + 1 : count = 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
