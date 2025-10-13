6e886661a03c2fb203a07c85e46b91a4985993cf
Zabadooj <51298182+Zabadoooj@users.noreply.github.com>
2025-10-13 12:06:38 +0300

    upd

--- module_dev-messenger/src/App.tsx
+++ module_dev-messenger/src/App.tsx
@@ -12,8 +12,13 @@ type ButtonProps = {
   backroungColor?: String;
 }
 
+type userCardProps = {
+  userName: String;
+  userDesc?: String;
+}
+
 const Input = (props: InputProps) => {
-  return(
+  return (
     <div className="input">
       <div className="input-name">
         {props.alias}
@@ -27,9 +32,18 @@ const Input = (props: InputProps) => {
 
 const Button = (props: ButtonProps) => {
   return (
-    <div className="button">
+    <div className="button" style={{ backgroundColor: props.backroungColor }}>
       {props.title}
-      
+    </div >
+  )
+}
+
+const UserCard = (props: userCardProps) => {
+  return (
+    <div className="user-Card">
+      <div className="user-Avatar"></div>
+      <div className="user-Name">{props.userName}</div>
+      <div className="user-Desc">{props.userDesc}</div>
     </div>
   )
 }
@@ -38,12 +52,19 @@ function App() {
 
   return (
     <>
-      <div className="film-creation-form">
-        <div className="text">Text</div>
-        <Input alias={"Логин"} textAlias={"Введите логин"}/>
-        <Input alias={"Почта"} textAlias={"Введите почту"}/>
-        <Input alias={"Пароль"} textAlias={"Введите пароль"}/>
-      </div>
+      <main>
+        <div className="film-creation-form">
+          <div className="text">Text</div>
+          <Input alias={"Логин"} textAlias={"Введите логин"} />
+          <Input alias={"Почта"} textAlias={"Введите почту"} />
+          <Input alias={"Пароль"} textAlias={"Введите пароль"} />
+          <Input alias={"Описание (не обязательно)"} textAlias={"Введите описание"} />
+
+          <Button title={"Button"} backroungColor={"white"} />
+        </div>
+
+        <UserCard userName={"Name"} userDesc={"Desc"} />
+      </main>
     </>
   )
 }
