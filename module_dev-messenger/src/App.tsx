fca7b0ce84938415ad0df72e84c46979a28b5ec9
Zabadooj <51298182+Zabadoooj@users.noreply.github.com>
2025-10-15 17:25:17 +0300

    upd

--- module_dev-messenger/src/App.tsx
+++ module_dev-messenger/src/App.tsx
@@ -1,71 +1,105 @@
 // import { useState } from 'react'
+import { useShallow } from 'zustand/shallow'
 import './App.css'
+import { useProfile } from './stores/profile_store'
+import { useState } from 'react'
 
 type InputProps = {
-  alias: String;
-  textAlias: String;
-  defaultValue?: String;
+  alias: String
+  textAlias: String
+  defaultValue?: String
+  onChange?: React.FormEventHandler<HTMLDivElement>
 }
 
 type ButtonProps = {
-  title: String;
-  backroungColor?: String;
-}
-
-type userCardProps = {
-  userName: String;
-  userDesc?: String;
+  title: String
+  backroungColor?: String
+  onClick: React.MouseEventHandler<HTMLDivElement>
 }
 
 const Input = (props: InputProps) => {
+  const [currentInput, setCurrentInput] = useState<string>('')
+
   return (
     <div className="input">
       <div className="input-name">
         {props.alias}
       </div>
-      <div className="inputbox" contentEditable>
-        {props.textAlias}
+      <div style={{
+        fontSize: "0.5em",
+        color: "rgb(129 129 129)",
+        position: "relative",
+        left: "13px",
+        marginBottom: "3px"
+      }}>
+        {!currentInput?"Ведите значение":null}
       </div>
+      <div className="inputbox" contentEditable onInput={
+        e=> {
+          props.onChange(e)
+          setCurrentInput(e.target.innerText)
+        }}>
+          
+        </div>
     </div>
   )
 }
 
 const Button = (props: ButtonProps) => {
   return (
-    <div className="button" style={{ backgroundColor: props.backroungColor }}>
+    <div className="button" style={{ backgroundColor: props.backroungColor }} onClick={props.onClick}>
       {props.title}
     </div >
   )
 }
 
 const UserCard = (props: userCardProps) => {
+
+  const {profileData} = useProfile(useShallow(state => ({
+    profileData: state.profileData
+  })))
+
   return (
     <div className="user-card">
       <div className="user-avatar"></div>
         <div className="user-data">
-          <div className="user-name">{props.userName}</div>
-          <div className="user-desc">{props.userDesc}</div>
+          <div className="user-name">{profileData.login}</div>
+          <div className="user-desc">{profileData.description}</div>
         </div>
     </div>
   )
 }
 
 function App() {
+  const {profileData, updateProfile} = useProfile(useShallow(state => ({
+    profileData: state.profileData,
+    updateProfile: state.updateProfile
+  }) ))
+
+  console.log(profileData, updateProfile)
 
   return (
     <>
       <main>
         <div className="film-creation-form">
           <div className="text">Text</div>
-          <Input alias={"Логин"} textAlias={"Введите логин"} />
+
+          <div style={{
+            flexDirection: "column",
+            display: "flex",
+            width: "90%",
+            gap: "30px"
+          }}>
+          <Input alias={"Логин"} textAlias={"Введите логин"} onChange={e => updateProfile("login", e.target.innerText)}/>
           <Input alias={"Почта"} textAlias={"Введите почту"} />
           <Input alias={"Пароль"} textAlias={"Введите пароль"} />
-          <Input alias={"Описание (не обязательно)"} textAlias={"Введите описание"} />
+          <Input alias={"Описание (не обязательно)"} textAlias={"Введите описание"} onChange={e=> updateProfile("description", e.target.innerText)}/>
+          </div>
 
-          <Button title={"Button"} backroungColor={"white"} />
+          <Button title={"Button"} backroungColor={"white"} onClick={e => console.log(profileData)}/>
         </div>
 
-        <UserCard userName={"Name"} userDesc={"Desc"} />
+        <UserCard />
       </main>
     </>
   )
